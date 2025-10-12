53786a81d4877ded206255ad5b8cc129e1a3135c
Gihan_Sachith <gihansachith92@gmail.com>
2025-10-12 23:17:08 +0530

    create payment entity

---
+++ backend/src/main/java/com/backend/model/Payment.java
@@ -0,0 +1,31 @@
+package com.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    private LocalDate paymentDate;
+    private LocalTime paymentTime;
+    private double amount;
+    private String paymentMethod;
+    private String paymentStatus;
+
+    @OneToOne
+    @JoinColumn(name = "booking_id")
+    private Booking booking;
+}
+
