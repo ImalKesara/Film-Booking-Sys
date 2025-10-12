e18c60073fa6fd6d31d1f751b9d83674c83e2c0f
Gihan_Sachith <gihansachith92@gmail.com>
2025-10-12 23:12:07 +0530

    create booking entity

---
+++ backend/src/main/java/com/backend/model/Booking.java
@@ -0,0 +1,36 @@
+package com.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    private LocalDateTime bookingTime;
+    private String bookingStatus;
+    private int sheetsBooked;
+    private double totalPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "show_id")
+    private Show show;
+
+    @OneToOne(mappedBy = "booking", cascade = CascadeType.ALL)
+    private Payment payment;
+}
+
