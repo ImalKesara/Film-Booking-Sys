91182697e9614d6c22510885a3e036d63bd38de5
Gihan_Sachith <gihansachith92@gmail.com>
2025-10-12 22:55:08 +0530

    create user entity

---
+++ backend/src/main/java/com/backend/model/User.java
@@ -0,0 +1,31 @@
+package com.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+    private LocalDate dateOfBirth;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Booking> bookings;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private LoyaltyPoint loyaltyPoint;
+}
