85ebef766e1da6f3869d6d0539aadd307ae1694f
Gihan_Sachith <gihansachith92@gmail.com>
2025-10-13 00:11:52 +0530

    create hall entity

---
+++ backend/src/main/java/com/backend/model/Hall.java
@@ -0,0 +1,32 @@
+package com.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Hall {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long hallId;
+
+    private String name;
+    private int totalSeats;
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    @OneToMany(mappedBy = "hall", cascade = CascadeType.ALL)
+    private List<Show> shows;
+}
+
