08abf979c3d3a247e6346f44931a88a774ba2ffc
Gihan_Sachith <gihansachith92@gmail.com>
2025-10-13 00:05:10 +0530

    create show entity

---
+++ backend/src/main/java/com/backend/model/Show.java
@@ -0,0 +1,39 @@
+package com.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Show {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long showId;
+
+    private LocalDate showDate;
+    private LocalTime showTime;
+    private double price;
+    private int availableSeats;
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "hall_id")
+    private Hall hall;
+
+    @OneToMany(mappedBy = "show", cascade = CascadeType.ALL)
+    private List<Booking> bookings;
+}
+
