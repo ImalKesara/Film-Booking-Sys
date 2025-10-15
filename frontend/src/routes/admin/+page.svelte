cadacfed9416f586fee41e58a693e8d8dfa31f9d
imalKesara <imalkesara2001@gmail.com>
2025-10-15 15:12:28 +0530

    feat: enhance movie pagination by adding current page tracking and total pages in admin view

--- frontend/src/routes/admin/+page.svelte
+++ frontend/src/routes/admin/+page.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import Movie from '$lib/components/Movie.svelte';
 	import type { MovieType } from '$lib/types';
-	let { data }: { data: { movies: MovieType[] } } = $props();
+	let { data }: { data: { movies: MovieType[]; totalPages: number; currentPage: number } } =
+		$props();
+	const goToPage = function (pNumber: number = 1) {
+		goto(`/admin?page=${pNumber}`);
+	};
 </script>
 
-<main class="mx-auto max-w-6xl space-y-8 p-4">
+<main class="mx-auto max-w-7xl space-y-8 p-4">
 	<h1 class="text-center text-lg">TMDB Movies</h1>
-	<div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+	<div class="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
 		{#each data.movies as movie (movie.id)}
 			<Movie {movie} />
 		{/each}
+	</div>
+	<div>
+		<button
+			class="btn preset-filled-primary-500 text-black"
+			onclick={() => goToPage(data.currentPage + 1)}>Next</button
+		>
 	</div>
 </main>
--- frontend/src/routes/admin/+page.ts
+++ frontend/src/routes/admin/+page.ts
@@ -1,8 +1,11 @@
 import type { PageLoad } from './$types';
 import { PUBLIC_TMDB_API_KEY } from '$env/static/public';
 
-export const load: PageLoad = async ({ fetch }) => {
+export const load: PageLoad = async ({ fetch, url }) => {
 	try {
+		const page = Number(url.searchParams.get('page') || '1');
+		console.log('Fetching upcoming movies for page:', page);
+
 		const options = {
 			method: 'GET',
 			headers: {
@@ -11,7 +14,10 @@ export const load: PageLoad = async ({ fetch }) => {
 			}
 		};
 
-		const res = await fetch(`https://api.themoviedb.org/3/movie/upcoming?page=1`, options);
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=${page}`,
+			options
+		);
 
 		if (!res.ok) {
 			throw new Error('Failed to fetch movie changes');
@@ -19,8 +25,12 @@ export const load: PageLoad = async ({ fetch }) => {
 
 		const data = await res.json();
 
+		console.log(data);
+
 		return {
-			movies: data.results || []
+			movies: data.results || [],
+			totalPages: data.total_pages || 1,
+			currentPage: page
 		};
 	} catch (error) {
 		console.error('Error fetching movie changes:', error);
