d3311d738a0e0cb7d32523f07729f62befd527bb
imalKesara <imalkesara2001@gmail.com>
2025-10-17 20:47:55 +0530

    Done themecontroller

--- frontend/src/lib/components/ThemeController.svelte
+++ frontend/src/lib/components/ThemeController.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
-	import { Switch } from '@skeletonlabs/skeleton-svelte';
+	import { MoonIcon, SunIcon } from '@lucide/svelte';
 
 	let checked: boolean = $state(false);
 
 	$effect(() => {
 		const mode = localStorage.getItem('mode') || 'light';
 		checked = mode === 'dark' ? true : false;
-		console.log('Theme mode set to:', mode);
 	});
 
-	function onCheckedChange(event: { checked: boolean }) {
-		const mode = event.checked ? 'dark' : 'light';
+	const toggle = () => {
+		checked = !checked;
+		const mode = checked ? 'dark' : 'light';
 		document.documentElement.setAttribute('data-mode', mode);
-	}
+		localStorage.setItem('mode', mode);
+	};
 </script>
 
 <svelte:head>
@@ -22,4 +23,10 @@
 	</script>
 </svelte:head>
 
-<Switch {checked} {onCheckedChange} name="mode"></Switch>
+<button class="btn-icon hover:preset-tonal" onclick={toggle}>
+	{#if checked}
+		<SunIcon class="size-5" />
+	{:else}
+		<MoonIcon class="size-5" />
+	{/if}
+</button>
