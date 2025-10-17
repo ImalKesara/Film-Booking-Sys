52a2ea0cc173f0ec3331e73171062c3424c520fa
imalKesara <imalkesara2001@gmail.com>
2025-10-17 18:16:31 +0530

    minor changes

--- frontend/src/routes/(dashboard)/+layout.svelte
+++ frontend/src/routes/(dashboard)/+layout.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 	let { children } = $props();
 	import favicon from '$lib/assets/favicon.svg';
+	import Footer from '$lib/components/Footer.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<Navbar />
+<main>
+	{@render children?.()}
+</main>
+<Footer />
