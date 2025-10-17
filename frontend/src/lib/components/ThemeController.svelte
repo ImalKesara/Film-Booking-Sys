7fa1039e0514073128c294e43d57a61ec1b7ba20
imalKesara <imalkesara2001@gmail.com>
2025-10-17 18:36:47 +0530

    light mode

---
+++ frontend/src/lib/components/ThemeController.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Switch } from '@skeletonlabs/skeleton-svelte';
+
+   let checked : boolean = $state(false)
+
+   $effect(()=>{
+      const mode = localStorage.getItem('mode') || 'light';
+      checked = mode === 'dark' ? true : false;
+   })
+
+   
+
+</script>
+
