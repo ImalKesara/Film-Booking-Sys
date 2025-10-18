6308f4fb6289b2646bb8b76c354d58e3c2fb4c00
Gihan_Sachith <gihansachith92@gmail.com>
2025-10-18 19:42:07 +0530

    add MovieRepository interface

---
+++ backend/src/main/java/com/backend/repository/MovieRepository.java
@@ -0,0 +1,7 @@
+package com.backend.repository;
+
+import com.backend.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+}
