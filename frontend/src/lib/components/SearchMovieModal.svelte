3afe62efb610870e938c2236141da6db4c6ad267
ImalKesara <imalkesara2001@gmail.com>
2025-10-23 19:20:06 +0530

    Search bar done

--- frontend/src/lib/components/SearchMovieModal.svelte
+++ frontend/src/lib/components/SearchMovieModal.svelte
@@ -1,40 +1,44 @@
 <script lang="ts">
 	import { Dialog, Portal } from '@skeletonlabs/skeleton-svelte';
 	let { isVisible = $bindable(), loading, movies } = $props();
+	import { fade } from 'svelte/transition';
 </script>
 
-<Dialog defaultOpen={isVisible} modal={true} >
+<Dialog open={isVisible} modal={true} onOpenChange={(d) => (isVisible = d.open)}>
 	<Portal>
 		<Dialog.Backdrop class="bg-surface-50-950/50 fixed inset-0 z-50" />
-		<Dialog.Positioner class="fixed inset-0 z-50 flex items-start mt-20 justify-center">
-			<Dialog.Content class="card bg-surface-100-900 w-md space-y-2 p-4 shadow-xl">
+		<Dialog.Positioner class="fixed inset-0 z-50 mt-20 flex items-start justify-center">
+			<Dialog.Content class="card bg-surface-100-900 w-4xl space-y-2 p-4 shadow-xl">
 				{#if loading}
 					Loading...
 				{:else if movies.length > 0 && movies}
-					{#each movies as movie}
-						<main
-							class="card preset-filled-surface-100-900 border-surface-200-800 card-hover divide-surface-200-800 block max-w-lg divide-y overflow-hidden border-[1px]"
-						>
-							<header>
-								<img
-									class="h-[250px] w-full object-cover"
-									src={`https://image.tmdb.org/t/p/original/${movie.posterUrl}`}
-									alt="Upcoming movies"
-								/>
-							</header>
-
-							<article class="space-y-4 p-4">
-								<div>
-									<h2 class="line-clamp-1 text-sm text-gray-500">{movie.title}</h2>
-								</div>
-							</article>
-						</main>
-					{/each}
+					<div class="grid grid-cols-5 gap-x-2">
+						{#each movies as movie}
+							<div class="card preset-tonal-primary-500 p-4">
+								<header>
+									<img
+										class="h-[250px] w-full object-cover"
+										src={`https://image.tmdb.org/t/p/original/${movie.posterUrl}`}
+										alt="Upcoming movies"
+									/>
+								</header>
+								<article class="space-y-4 p-4">
+									<div>
+										<h2 class="h6 text-sm line-clamp-1">{movie.title}</h2>
+									</div>
+									<p class="text-xs opacity-60 line-clamp-2">
+										{movie.description}
+									</p>
+								</article>
+							</div>
+						{/each}
+					</div>
 				{:else}
 					No results found
 				{/if}
-				<Dialog.CloseTrigger class="btn preset-tonal" onclick={() => (isVisible = false)}
-					>Close</Dialog.CloseTrigger
+				<Dialog.CloseTrigger
+					class="btn preset-tonal"
+					onclick={() => (isVisible = false)}>Close</Dialog.CloseTrigger
 				>
 			</Dialog.Content>
 		</Dialog.Positioner>
