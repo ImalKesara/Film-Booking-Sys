0e5a54a129281a27feb5aedf2177ede1d8f76973
ImalKesara <imalkesara2001@gmail.com>
2025-10-27 09:32:54 +0530

    Line Charts

--- backend/src/main/java/com/backend/model/ShowSalesSummary.java
+++ backend/src/main/java/com/backend/model/ShowSalesSummary.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "v_show_sales_summary")
 public class ShowSalesSummary {
 
@@ -27,62 +29,6 @@ public class ShowSalesSummary {
 
     @Column(name = "totalSeatsBooked")
     private Integer totalSeatsBooked;
-
-    public Long getShowId() {
-        return showId;
-    }
-
-    public void setShowId(Long showId) {
-        this.showId = showId;
-    }
-
-    public String getShowDate() {
-        return showDate;
-    }
-
-    public void setShowDate(String showDate) {
-        this.showDate = showDate;
-    }
-
-    public String getShowTime() {
-        return showTime;
-    }
-
-    public void setShowTime(String showTime) {
-        this.showTime = showTime;
-    }
-
-    public String getMovieTitle() {
-        return movieTitle;
-    }
-
-    public void setMovieTitle(String movieTitle) {
-        this.movieTitle = movieTitle;
-    }
-
-    public String getHallName() {
-        return hallName;
-    }
-
-    public void setHallName(String hallName) {
-        this.hallName = hallName;
-    }
-
-    public Integer getTotalSeatsBooked() {
-        return totalSeatsBooked;
-    }
-
-    public void setTotalSeatsBooked(Integer totalSeatsBooked) {
-        this.totalSeatsBooked = totalSeatsBooked;
-    }
-
-    public Double getTotalRevenue() {
-        return totalRevenue;
-    }
-
-    public void setTotalRevenue(Double totalRevenue) {
-        this.totalRevenue = totalRevenue;
-    }
 
     @Column(name = "totalRevenue")
     private Double totalRevenue;
