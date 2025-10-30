ca14d3eeecc9f0cf34200f0cc041cdb8ceb369f9
Gihan_Sachith <gihansachith92@gmail.com>
2025-10-31 00:40:03 +0530

    notification entity

---
+++ backend/src/main/java/com/backend/model/Notification.java
@@ -0,0 +1,27 @@
+package com.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Notification")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private String notificationMessage;
+
+    private LocalDateTime createdAt;
+
+}
